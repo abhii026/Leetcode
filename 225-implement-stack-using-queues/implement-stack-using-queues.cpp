77bfda7aa199227118c6a242bbd59dbb5f770f2e
Abhishek Singh <iabhii026@gmail.com>
2025-12-02 13:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (18.69%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,18 +1,27 @@
 class MyStack {
 public:
     queue<int> q1;
-    queue<int> q2;
+    // queue<int> q2;
     MyStack() {
         
     }
     
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
+        // q2.push(x);
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // swap(q1,q2);
+        q1.push(x);
+        int n= q1.size();
+        while(n>1){
+            q1.push(q1.front());
             q1.pop();
+            n--;
+
         }
-        swap(q1,q2);
+
     }
     
     int pop() {
