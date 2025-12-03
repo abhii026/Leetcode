434a080ecbaedc26d8ab91daf729e97c01c74eb9
Abhishek Singh <iabhii026@gmail.com>
2025-12-03 15:32:33 +0530

    Time: 19 ms (26.27%) | Memory: 20.5 MB (8.29%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-         int freq[26] = {0};
-        for(int i = 0; i < s.size(); i++)
+        //  int freq[26] = {0};
+        // for(int i = 0; i < s.size(); i++)
+        //     freq[s[i] - 'a']++;
+        // for(int i = 0; i < s.size(); i++)
+        //     if(freq[s[i] - 'a'] == 1) return i;
+        // return -1;
+        int freq[26] = {0};
+        queue<int> q;
+
+        for(int i = 0; i < s.size(); i++){
             freq[s[i] - 'a']++;
-        for(int i = 0; i < s.size(); i++)
-            if(freq[s[i] - 'a'] == 1) return i;
-        return -1;
+            q.push(i);
+
+            while(!q.empty() && freq[s[q.front()] - 'a'] > 1)
+                q.pop();
+        }
+
+        if(q.empty()) return -1;
+        return q.front();
     }
 };
